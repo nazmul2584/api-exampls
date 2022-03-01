eb1acacbf9b5ce02935838268148ed9981533438
nazmul <nazmulalam9203@gmail.com>
2022-03-01 20:12:31 +0600

    aaa

--- mealdb.js
+++ mealdb.js
@@ -23,7 +23,7 @@ const displayserchresult = meals => {
      
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
+        <div onclick="loadmealdetails(${meal.idMeal})" class="card h-100">
           <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}  </h5>
@@ -37,3 +37,30 @@ const displayserchresult = meals => {
     
 }
 
+const loadmealdetails= mealid =>{
+const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid} `;
+fetch(url)
+.then(res => res.json())
+.then(data => displydetails(data.meals[0]));
+}
+
+
+const displydetails = meal =>{
+
+    console.log(meal);
+    const mealdetails = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML= `
+
+    <img src="${meal.strMealThumb} " class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal} </h5>
+      <p class="card-text">${meal.strInstructions.slice(0,150)} </p>
+      <a href="${meal.stryoutube}" class="btn btn-primary">Go somewhere</a>
+    </div>
+    
+    `;
+    mealdetails.appendChild(div)
+
+} 
